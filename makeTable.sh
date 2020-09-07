e86f9537b2803d3c46e6a09e3f2d2fa42a22425b
runt9 <runt9ab@gmail.com>
2020-09-07 13:25:03 -0500

    Updated makeTable.sh

--- makeTable.sh
+++ makeTable.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
+TMP_CSVFILE=.tmpcsv.csv
 csvFile=$1
+
+cat $csvFile | tr -d '\r' > $TMP_CSVFILE
 
 echo "[table]"
 echo "[thead]"
 echo "[tr]"
 
-for h in $(head -1 $csvFile | tr ',' ' '); do
-    echo "[th]$h[/th]"
+for h in $(head -1 $TMP_CSVFILE | tr ' ' '_' | tr ',' ' '); do
+    echo "[th]$h[/th]" | tr '_' ' '
 done
 
 echo "[/tr]"
 echo "[/thead]"
 
 echo "[tbody]"
-for row in $(tail -n+2 $csvFile); do
+for row in $(tail -n+2 $TMP_CSVFILE); do
     echo "[tr]"
-    for cell in $(echo "$row" | tr ',' ' '); do
-        echo "[td]$cell[/td]"
+    for cell in $(echo "$row" | tr ' ' '_' | tr ',' ' '); do
+        echo "[td]$cell[/td]" | tr '_' ' '
     done
     echo "[/tr]"
 done
 echo "[/tbody]"
 echo "[/table]"
+
+rm $TMP_CSVFILE
