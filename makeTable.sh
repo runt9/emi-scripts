5a1e9598bcb2c7cfc5c6b54d95d395e183fadc65
runt9 <runt9ab@gmail.com>
2020-09-07 17:35:27 -0500

    More fixes to makeTable.sh

--- makeTable.sh
+++ makeTable.sh
@@ -3,13 +3,13 @@
 TMP_CSVFILE=.tmpcsv.csv
 csvFile=$1
 
-cat $csvFile | tr -d '\r' > $TMP_CSVFILE
+cat $csvFile | tr -d '\r' | tr '\t' '|' > $TMP_CSVFILE
 
 echo "[table]"
 echo "[thead]"
 echo "[tr]"
 
-for h in $(head -1 $TMP_CSVFILE | tr ' ' '_' | tr ',' ' '); do
+for h in $(head -1 $TMP_CSVFILE | tr ' ' '_' | tr '|' ' '); do
     echo "[th]$h[/th]" | tr '_' ' '
 done
 
@@ -17,9 +17,9 @@ echo "[/tr]"
 echo "[/thead]"
 
 echo "[tbody]"
-for row in $(tail -n+2 $TMP_CSVFILE); do
+for row in $(tail -n+2 $TMP_CSVFILE | tr ' ' '_'); do
     echo "[tr]"
-    for cell in $(echo "$row" | tr ' ' '_' | tr ',' ' '); do
+    for cell in $(echo "$row" | tr ' ' '_' | tr '|' ' '); do
         echo "[td]$cell[/td]" | tr '_' ' '
     done
     echo "[/tr]"
